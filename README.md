f9397c5af092af375ca88a55437f340e07603e1d
Reinhard Stahn <reinhardstahn@t-online.de>
2022-08-14 16:42:02 +0200

    Complete rewrite of the game

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# Quantum Battleships (by James Wootton)
+# Quantum Battleships (originally by James Wootton)
 
-This repo contains the game `Quantum Battleships` by James Wootton as described in [this medium article](https://decodoku.medium.com/quantum-battleships-the-first-multiplayer-game-for-a-quantum-computer-e4d600ccb3f3). We took the script from the google drive linked to from there.
+This repo contains the game `Quantum Battleships` originally devised by James Wootton. His original python script was "published" in [this medium article](https://decodoku.medium.com/quantum-battleships-the-first-multiplayer-game-for-a-quantum-computer-e4d600ccb3f3).
+
+We heavily refactored the script. Most notably we
+- reorganized the code structure,
+- tweaked the code to be compatible with a newer version of `projectq`,
+- use `projectq`'s `Simulator` backend instead of the `IBMBackend` for simplicity.
+
+Moreover we added an option to not play the game but to instead print the expected outcomes of all possible scenarios (there are not that many ways the game can be played). Just set `play_game = False` (see the config section of the script).
+
+You might be disappointed that we just use a simulator. But it is actually easy to tweak the script to use any other backend, including any real quantum computer. See [the docs of projectq](https://projectq.readthedocs.io/en/latest/index.html) on how to do that. I did not do that because I found it to be overkill to register with a provider of quantum computation just to try out a little game.
+
+The game is still not very fancy. I just did the rewrite for my own pleasure while studying the theory behind the game - which in contrast is very fascinating (for me). Feel free to use the script for whatever purpose. I doubt that the original author has anything against it.
+
+## Setup
+
+To play the game we suggest to create a virtual environment for python. Make sure you have a recent version of `python3` and do the following (here and in the following I demonstrate the steps as it would be done in a "usual" Linux distro):
+
+```shell
+$ # Open the terminal and navigate to the directory of this README.
+$ python -m venv .venv
+```
+
+This creates a directory `.venv`. Activate the virtual environment by typing
+
+```shell
+$ source .venv/bin/activate
+(.venv) $ # Usually this changes the command-prompt like here.
+```
+
+Now, if you type `python` the interpreter from that folder should be used. If in doubt you could use the command `type python` and check if the path points to a binary within the `.venv` folder.
+
+Next we install the required packages:
+
+```shell
+(.venv) $ python -m pip install -r requirements.txt
+```
+
+## Play the game
+
+After you successfully completed the setup you can start and play the game by running
+
+```shell
+(.venv) $ python quantum-battleships.py
+```
+
+Note that you have to run the script from within the virtual environment (see setup).
+
+There is a little section `global configruation` at the top of the script. You can adjust the parameters to tweak the behavior of the game. In particular you can print out the (statistically) expected outcomes of all possible ways the game can be played by setting `play_game = False`.
--- quantum-battleships.py
+++ quantum-battleships.py
@@ -1,215 +1,267 @@
-import math, numpy, random, time, getpass # normal python stuff
+import itertools, math, time, getpass # normal python stuff
 
-from projectq import MainEngine  # import the main compiler engine
-from projectq.ops import H, S, T, X, CNOT, Entangle, get_inverse, Measure  # import the operations we want to perform
+from projectq import MainEngine, types  # import the main compiler engine
+from projectq.ops import H, S, T, X, CNOT, get_inverse, Measure  # import the operations we want to perform
 
-import projectq.setups.ibm
-from projectq.backends import IBMBackend
+###############################################################################
+### global configuration
+###############################################################################
 
-eng = MainEngine(IBMBackend(use_hardware=True, num_runs=1024, verbose=False, user=None, password=None))
+play_game = False
+timeout = 1 # seconds
+num_samples_for_game = 1000
 
-print("\n\n\n\n===== Welcome to Quantum Battleships! =====\n\n")
-print("  ~~ A game by the Decodoku project ~~ ")
-print("\n\n")
-print("When in doubt, press any key to continue!")
-raw_input()
-print("This is a game for two players.")
-raw_input()
-print("Player 1 will choose the position of a Battleship.")
-raw_input()
-print("Player 2 will try to bomb it.")
-raw_input()
+###############################################################################
+### global constants
+###############################################################################
 
-# get player 1 to position boat
-print("We start with Player 1.")
-print("Look away Player 2!")
-raw_input()
-print("The lines in the bowtie shape below are the places you can place your ship.\n")
-print("|\     /|")
-print("| d   b |")
-print("|  \ /  |")
-print("f   X   a")
-print("|  / \  |")
-print("| e   c |")
-print("|/     \|\n")
-# note: at time of release, ProjectQ does not actually put the qubits in the places you'd expect on the IBM chip
-raw_input()
+INVALID_INPUT_MSG = "u wot m8? Try that again."
 
-chosen = 0
-while (chosen==0):
-	ship = getpass.getpass("Choose a line for your ship. (a, b, c, d, e or f)\n")
-	if ship in ["a","b","c","d","e","f"]:
-		chosen = 1
-	else:
-		print("u wot m8? Try that again.")
+# A ship (its positions) is represented by two of five qubits.
+ship_bit_positions = dict(
+    a = [0, 1],
+    b = [0, 2],
+    c = [1, 2],
+    d = [2, 4],
+    e = [2, 3],
+    f = [3, 4],
+)
 
-# get player 2 to position three bombs
-time.sleep(1)
-print("\nPlayer 2: You're up!")
-raw_input()
-print("The numbers below mark places you can bomb.\n")
-print("4       0")
-print("|\     /|")
-print("| \   / |")
-print("|  \ /  |")
-print("|   2   |")
-print("|  / \  |")
-print("| /   \ |")
-print("|/     \|")
-print("3       1\n")
-raw_input()
+###############################################################################
+### Auxiliary functions
+###############################################################################
 
-chosen = 0
-while (chosen==0):
-    bomb1 = int(raw_input("Choose a position for your first bomb. (0, 1, 2, 3 or 4)\n"))
-    if ( (bomb1 >= 0) & (bomb1 < 5) ):
-        chosen = 1
-    else:
-        print("u wot m8? Try that again.")
+def display_intro():
+    print("\n\n\n\n===== Welcome to Quantum Battleships! =====\n\n")
+    print("  ~~ A game by the Decodoku project ~~ ")
+    print("\n\n")
+    print("When in doubt, press any key to continue!")
+    input()
+    print("This is a game for two players.")
+    input()
+    print("Player 1 will choose the position of a Battleship.")
+    input()
+    print("Player 2 will try to bomb it.")
+    input()
 
-chosen = 0
-while (chosen==0):
-    bomb2 = int(raw_input("\nChoose a position for your second bomb. (0, 1, 2, 3 or 4)\n"))
-    if ( (bomb1 >= 0) & (bomb1 < 5) ):
-        if (bomb2 != bomb1):
-            chosen = 1
+def display_intro_player_1():
+    # get player 1 to position boat
+    print("We start with Player 1.")
+    print("Look away Player 2!")
+    input()
+    print("The lines in the bowtie shape below are the places you can place your ship.\n")
+    print("|\     /|")
+    print("| d   b |")
+    print("|  \ /  |")
+    print("f   X   a")
+    print("|  / \  |")
+    print("| e   c |")
+    print("|/     \|\n")
+    # note: at time of release, ProjectQ does not actually put the qubits in the places you'd expect on the IBM chip
+    input()
+
+def display_intro_player_2():
+    # get player 2 to position three bombs
+    time.sleep(timeout)
+    print("\nPlayer 2: You're up!")
+    input()
+    print("The numbers below mark places you can bomb.\n")
+    print("4       0")
+    print("|\     /|")
+    print("| \   / |")
+    print("|  \ /  |")
+    print("|   2   |")
+    print("|  / \  |")
+    print("| /   \ |")
+    print("|/     \|")
+    print("3       1\n")
+    input()
+
+def display_intermidiate_message():
+    time.sleep(timeout)
+    print("\nNow let's see how intact the ship is.")
+    print("Between 1% and 100% intact means it's still afloat.")
+    print("Between -1% and -100% intact means it's swimming with the fishes.")
+    print("0% intact could go either way.\n")
+
+def display_results(damage, rounding = False):
+    time.sleep(timeout)
+    conv = int if rounding else float # crude way to round
+    print(f"The ship is {conv( 100*(1 - 2*damage) )}% intact")
+    if play_game:
+        print(f"(which means {conv( -100*(1 - 2*damage) )}% broken).\n")
+        if (damage > 0.5):
+            print("It has been destroyed!\nPlayer 2 wins!\n\n")
         else:
-            print("That's already been bombed. Choose again.")
-    else:
-        print("u wot m8? Try that again.")
+            print("It's still afloat!\nPlayer 1 wins!\n\n")
 
-chosen = 0
-while (chosen==0):
-    bomb3 = int(raw_input("\nChoose a position for your third and final bomb. (0, 1, 2, 3 or 4)\n"))
-    if ( (bomb1 >= 0) & (bomb1 < 5) ):
-        if bomb3 not in [bomb1,bomb2]:
-            chosen = 1
+
+def prompt_for_ship(default_ship = None):
+    """Ask user for the ship. If default_ship is given skip asking user."""
+    available_ships = ship_bit_positions.keys()
+
+    if default_ship is not None and default_ship in available_ships:
+        return default_ship
+
+    while True:
+        ship = getpass.getpass(f"Choose a line for your ship. ({', '.join(available_ships)})\n")
+        if ship in available_ships:
+            return ship
         else:
-            print("That's already been bombed. Choose again.")
+            print(INVALID_INPUT_MSG)
+
+
+def prompt_for_bombs(default_bombs = None):
+    """Ask user for bombs. If default_bombs is given skip asking user."""
+    bombs = []
+    if default_bombs is None: default_bombs = []
+
+    ordinals = ["first", "second"]
+
+    while (len(bombs) < 2):
+        bomb = default_bombs[len(bombs)] if len(bombs) < len(default_bombs) else None
+
+        if bomb is None:
+            try:
+                bomb = int(input(f"\nChoose a position for your {ordinals[len(bombs)]} bomb. (0, 1, 2, 3 or 4)\n"))
+            except:
+                print(INVALID_INPUT_MSG)
+                continue
+
+        if (bomb >= 0) and (bomb < 5) :
+            if bomb not in bombs:
+                bombs.append(bomb)
+            else:
+                print("That's already been bombed. Choose again.")
+        else:
+            print(INVALID_INPUT_MSG)
+
+    return bombs
+
+
+def entangle_CHSH(qubits, bobs, i, j):
+    """Make entangled pair of qubits prepared for a CHSH experiment.
+
+    qubits and bobs are output parameters. i and j are the desired positions of
+    the qubits representing the ship.
+    """
+    # Make Bell-Pair:
+    H | qubits[i]
+    CNOT | (qubits[i], qubits[j])
+    # Use an X to anticocorrelate Z basis (measurements will be made in x-y
+    # plane of the Bloch-Sphere):
+    X | qubits[i]
+    # Do a phase-shift of angle pi/4 on any Bobs
+    bobs[i] = 1
+    T | qubits[i]
+
+
+def get_probabilities(qubits: types.Qureg):
+    eng = qubits.engine
+    return { bit_string: eng.backend.get_probability(bit_string, qubits)
+        for bit_string in itertools.product(*5*[[0, 1]])}
+
+
+def run_scenario(eng, ship, bombs, do_measurements = True):
+    qubits = eng.allocate_qureg(5)
+    bobs = 5 * [0] # 0="no bob", 1="is bob".
+
+    # prepare the two ship-qubits and mark which one is the bob
+    entangle_CHSH(qubits, bobs, *ship_bit_positions[ship])
+
+    # apply the bombs
+    # whether or not a bomb is applied corresponds to the two measurment choices for CHSH (in x-y plane)
+    for bomb in bombs:
+        if (bobs[bomb] == 1):
+            get_inverse(S) | qubits[bomb]
+        else:
+            S | qubits[bomb]
+
+    # TODO: Better name
+    results = None
+
+    if do_measurements:
+        # measure should be done in X basis, therefore we apply Hadamard here
+        for i in range(5):
+            H | qubits[i]
+            Measure | qubits[i]
+        eng.flush()  # flush all gates (and execute measurements)
+
+        # get measurement results (note: the keys are tuples)
+        results = get_probabilities(qubits) # discrete results (in theory only zeros or ones)
     else:
-        print("u wot m8? Try that again.")
+        for i in range(5):
+            H | qubits[i]
+            # NOTE: No measurement!
+        eng.flush()
 
-# now all that's left is to run the scenario on the qubits and see what happens
-print("\nWe'll now run this scenario on IBM's qubits and see what happens.")
-raw_input()
-print("But first you'll have to sign in...\n")
+        # Here we immediately get the theoretical probabilities
+        results = get_probabilities(qubits)
 
+        for i in range(5):
+            # Dummy-measurement since projectq complains about it if we leave it out
+            Measure | qubits[i]
+        eng.flush()
 
-# prepare qubits
-qubits = eng.allocate_qureg(5)
+    return results
 
-# make the ship: an entangled pairs prepared for a CHSH experiment
-# first we make a Bell pair with H and CNOT
-# then use an X to anticocorrelate Z basis (measurements will be made in x-y plane)
-# then do a T on any Bobs (higher numbered qubit)
-bobs = [0]*5 
-if (ship == "a"): # a means 0 and 1
-	H | qubits[0]
-	CNOT | (qubits[0], qubits[1])
-	X | qubits[0]
-	bobs[0] = 1
-	T | qubits[0]
-if (ship == "b"): # b means 0 and 2
-	H | qubits[0]
-	CNOT | (qubits[0], qubits[2])
-	X | qubits[0]
-	bobs[0] = 1
-	T | qubits[0]
-if (ship == "c"): # c means 1 and 2
-	H | qubits[1]
-	CNOT | (qubits[1], qubits[2])
-	X | qubits[1]
-	bobs[1] = 1
-	T | qubits[1]
-if (ship == "d"): # d means 2 and 4
-	H | qubits[4]
-	CNOT | (qubits[4], qubits[2])
-	X | qubits[4]
-	bobs[2] = 1
-	T | qubits[2]
-if (ship == "e"): # e means 2 and 3
-	H | qubits[3]
-	CNOT | (qubits[3], qubits[2])
-	X | qubits[3]
-	bobs[2] = 1
-	T | qubits[2]
-if (ship == "f"): # f means 3 and 4
-	H | qubits[3]
-	CNOT | (qubits[3], qubits[4])
-	X | qubits[3]
-	bobs[3] = 1
-	T | qubits[3]
+###############################################################################
+### Main code
+###############################################################################
 
-# apply the bombs
-# whether or not a bomb is applied corresponds to the two measurment choices for CHSH (in x-y plane)
-if (bobs[bomb1]==1):
-	get_inverse(S) | qubits[bomb1]
-else:
-	S | qubits[bomb1]
-if (bobs[bomb2]==1):
-	get_inverse(S) | qubits[bomb2]
-else:
-	S | qubits[bomb2]
+def main(ship = None, bombs = None):
+    eng = MainEngine() # Uses a simulation of a quantum computer
 
-# measure all in X basis
-H | qubits[0]
-Measure | qubits[0]
-H | qubits[1]
-Measure | qubits[1]
-H | qubits[2]
-Measure | qubits[2]
-H | qubits[3]
-Measure | qubits[3]
-H | qubits[4]
-Measure | qubits[4]
+    if play_game: display_intro()
 
-eng.flush()  # flush all gates (and execute measurements)
+    if play_game: display_intro_player_1()
+    ship = prompt_for_ship(ship)
 
-time.sleep(1)
-print("\nNow let's see how intact the ship is.")
-print("Between 1% and 100% intact means it's still afloat.")
-print("Between -1% and -100% intact means it's swimming with the fishes.")
-print("0% intact could go either way.")
+    if play_game: display_intro_player_2()
+    bombs = prompt_for_bombs(bombs)
 
-results = eng.backend.get_probabilities(qubits) # get probabilities
-# fill in mising values
-for b1 in range(2):
-	for b2 in range(2):
-		for b3 in range(2):
-			for b4 in range(2):
-				for b5 in range(2):
-					bitString = str(b1) + str(b2) + str(b3) + str(b4) + str(b5)
-					if bitString not in results:
-						results[bitString] = 0
+    # If we do not play the game we directly get the probabilities, hence we do
+    # not need more then one run.
+    num_samples = num_samples_for_game if play_game else 1
 
-# determine damage for ship
-damage = 0
-for b1 in range(2):
-	for b2 in range(2):
-		for b3 in range(2):
+    if play_game: print(f"\nWe'll now run {num_samples} samples of this scenario and see what happens.")
 
-			if (ship == "a"): # a means 0 and 1
-				damage = damage + results[ "01" + str(b1) + str(b2) + str(b3) ] + results[ "10" + str(b1) + str(b2) + str(b3) ]
-			if (ship == "b"): # b means 0 and 2
-				damage = damage + results[ "0" + str(b1) + "1" + str(b2) + str(b3) ] + results[ "1" + str(b1) + "0" + str(b2) + str(b3) ]
-			if (ship == "c"): # c means 1 and 2
-				damage = damage + results[ str(b1) + "01" + str(b2) + str(b3) ] + results[ str(b1) + "10" + str(b2) + str(b3) ]
-			if (ship == "d"): # d means 2 and 4
-				damage = damage + results[ str(b1) + str(b2) + "0" + str(b3) + "1" ] + results[ str(b1) + str(b2) + "1" + str(b3) + "0" ]
-			if (ship == "e"): # e means 2 and 3
-				damage = damage + results[ str(b1) + str(b2) + "01" + str(b3) ] + results[ str(b1) + str(b2) + "10" + str(b3) ]
-			if (ship == "f"): # f means 3 and 4
-				damage = damage + results[ str(b1) + str(b2) + str(b3) + "01" ] + results[ str(b1) + str(b2) + str(b3) +"10" ]
+    # Not sure if this loop is really necessary, but the approach from the
+    # original script didn't work anymore (for the Simulator backend).
+    results = dict()
+    for i in range(1, num_samples + 1):
+        if play_game: print(f"{int(100*(i/num_samples)):3}% Done.", end="\r" if i != num_samples else "\n")
+        r = run_scenario(eng, ship, bombs, do_measurements=play_game)
+
+        for bs, prob in r.items():
+            results.setdefault(bs, 0.0)
+            results[bs] += prob / num_samples
+
+    if play_game: display_intermidiate_message()
+
+    # determine damage for ship
+    damage = 0
+    for bs0 in itertools.product(*3*[[0, 1]]):
+        for bits in [[0, 1], [1, 0]]:
+            bit_string = list(bs0)
+            bit_string.insert(ship_bit_positions[ship][0], bits[0])
+            bit_string.insert(ship_bit_positions[ship][1], bits[1])
+            bit_string = tuple(bit_string)
+            damage += results[bit_string]
+
+    display_results(damage, rounding=play_game)
 
 
-time.sleep(1)
-print("\nThe ship is " + str(int( 100*(1-2*damage) )) + "% intact")
-print("(which means " + str(int( -100*(1-2*damage) )) + "% broken).\n")
-if (damage>0.5):
-	print("It has been destroyed!\nPlayer 2 wins!\n\n")
-else:
-    print("It's still afloat!\nPlayer 1 wins!\n\n")
-
-
-
-
+if __name__ == "__main__":
+    if play_game:
+        main()
+    else:
+        timeout = 0
+        ships = ship_bit_positions.keys()
+        #ships = ["a"]
+        for ship in ships:
+            bomb_combis = itertools.combinations([0, 1, 2, 3, 4], 2)
+            for bombs in bomb_combis:
+                print(f"ship={ship}, bombs={bombs}", end=": ")
+                main(ship, bombs)
+            print()
